9a3603aaf44a3ee9304f4cb39244418842b857ca
Nami-Webisoft <169168024+Nami-Webisoft@users.noreply.github.com>
2024-07-29 10:32:54 -0400

    Update constants.py

--- ecommerce_integrations/shopify/constants.py
+++ ecommerce_integrations/shopify/constants.py
@@ -6,7 +6,7 @@ MODULE_NAME = "shopify"
 SETTING_DOCTYPE = "Shopify Setting"
 OLD_SETTINGS_DOCTYPE = "Shopify Settings"
 
-API_VERSION = "2024-01"
+API_VERSION = "2023-10"
 
 WEBHOOK_EVENTS = [
 	"orders/create",
